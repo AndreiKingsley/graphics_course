c3a8a00f4072a664f2092248e97cf8c25b84c471
Andrew <kisandy@yandex.ru>
2021-09-21 11:48:22 +0300

    prac 3 init

--- practice1/main.cpp
+++ practice1/main.cpp
@@ -11,6 +11,7 @@
 #include <stdexcept>
 #include <iostream>
 #include <chrono>
+#include <vector>
 
 std::string to_string(std::string_view str)
 {
@@ -29,31 +30,24 @@ void glew_fail(std::string_view message, GLenum error)
 
 const char vertex_shader_source[] =
         R"(#version 330 core
-const vec2 VERTICES[3] = vec2[3](
-	vec2(0.0, 0.0),
-	vec2(1.0, 0.0),
-	vec2(0.0, 1.0)
-);
-out vec3 color;
-
 uniform mat4 view;
-uniform mat4 transform;
-
+layout (location = 0) in vec2 in_position;
+layout (location = 1) in vec4 in_color;
+out vec4 color;
 void main()
 {
-	vec2 position = VERTICES[gl_VertexID];
-	gl_Position = view * transform * vec4(position, 0.0, 1.0);
-	color = vec3(position, 0.0);
+	gl_Position = view * vec4(in_position, 0.0, 1.0);
+	color = in_color;
 }
 )";
 
 const char fragment_shader_source[] =
         R"(#version 330 core
-in vec3 color;
+in vec4 color;
 layout (location = 0) out vec4 out_color;
 void main()
 {
-	out_color = vec4(color, 1.0);
+	out_color = color;
 }
 )";
 
@@ -96,12 +90,48 @@ GLuint create_program(GLuint vertex_shader, GLuint fragment_shader)
     return result;
 }
 
+struct vec2
+{
+    float x;
+    float y;
+};
+
+struct vertex
+{
+    vec2 position;
+    std::uint8_t color[4];
+};
+
+vec2 bezier(std::vector<vertex> const & vertices, float t)
+{
+    std::vector<vec2> points(vertices.size());
+
+    for (std::size_t i = 0; i < vertices.size(); ++i)
+        points[i] = vertices[i].position;
+
+    // De Casteljau's algorithm
+    for (std::size_t k = 0; k + 1 < vertices.size(); ++k) {
+        for (std::size_t i = 0; i + k + 1 < vertices.size(); ++i) {
+            points[i].x = points[i].x * (1.f - t) + points[i + 1].x * t;
+            points[i].y = points[i].y * (1.f - t) + points[i + 1].y * t;
+        }
+    }
+    return points[0];
+}
+
 int main() try
 {
     if (SDL_Init(SDL_INIT_VIDEO) != 0)
         sdl2_fail("SDL_Init: ");
 
-    SDL_Window * window = SDL_CreateWindow("Graphics course practice 1",
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 3);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+    SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+    SDL_GL_SetAttribute(SDL_GL_MULTISAMPLEBUFFERS, 1);
+    SDL_GL_SetAttribute(SDL_GL_MULTISAMPLESAMPLES, 4);
+
+    SDL_Window * window = SDL_CreateWindow("Graphics course practice 3",
                                            SDL_WINDOWPOS_CENTERED,
                                            SDL_WINDOWPOS_CENTERED,
                                            800, 600,
@@ -113,16 +143,11 @@ int main() try
     int width, height;
     SDL_GetWindowSize(window, &width, &height);
 
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 3);
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
-    SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
-
     SDL_GLContext gl_context = SDL_GL_CreateContext(window);
-
-    SDL_GL_SetSwapInterval(0);
     if (!gl_context)
         sdl2_fail("SDL_GL_CreateContext: ");
+
+    SDL_GL_SetSwapInterval(0);
 
     if (auto result = glewInit(); result != GLEW_NO_ERROR)
         glew_fail("glewInit: ", result);
@@ -132,21 +157,15 @@ int main() try
 
     glClearColor(0.8f, 0.8f, 1.f, 0.f);
 
-    GLuint vertex_shader = create_shader(GL_VERTEX_SHADER, vertex_shader_source);
-    GLuint fragment_shader = create_shader(GL_FRAGMENT_SHADER, fragment_shader_source);
+    auto vertex_shader = create_shader(GL_VERTEX_SHADER, vertex_shader_source);
+    auto fragment_shader = create_shader(GL_FRAGMENT_SHADER, fragment_shader_source);
+    auto program = create_program(vertex_shader, fragment_shader);
 
-    GLuint program = create_program(vertex_shader, fragment_shader);
-    glUseProgram(program);
-
-    float time = 0.f;
-
-    auto transform_loc = glGetUniformLocation(program, "transform");
-    auto view_loc = glGetUniformLocation(program, "view");
-
-    GLuint vao;
-    glGenVertexArrays(1, &vao);
+    GLuint view_location = glGetUniformLocation(program, "view");
 
     auto last_frame_start = std::chrono::high_resolution_clock::now();
+
+    float time = 0.f;
 
     bool running = true;
     while (running)
@@ -165,50 +184,49 @@ int main() try
                             break;
                     }
                     break;
+                case SDL_MOUSEBUTTONDOWN:
+                    if (event.button.button == SDL_BUTTON_LEFT)
+                    {
+                        int mouse_x = event.button.x;
+                        int mouse_y = event.button.y;
+                    }
+                    else if (event.button.button == SDL_BUTTON_RIGHT)
+                    {
+
+                    }
+                    break;
+                case SDL_KEYDOWN:
+                    if (event.key.keysym.sym == SDLK_LEFT)
+                    {
+
+                    }
+                    else if (event.key.keysym.sym == SDLK_RIGHT)
+                    {
+
+                    }
+                    break;
             }
 
         if (!running)
             break;
 
         auto now = std::chrono::high_resolution_clock::now();
-       // float dt = std::chrono::duration_cast<std::chrono::duration<float>>(now - last_frame_start).count();
-        float dt = 0.016f;
+        float dt = std::chrono::duration_cast<std::chrono::duration<float>>(now - last_frame_start).count();
         last_frame_start = now;
-
-        std::cout << dt << std::endl;
         time += dt;
 
         glClear(GL_COLOR_BUFFER_BIT);
 
+        float view[16] =
+                {
+                        1.f, 0.f, 0.f, 0.f,
+                        0.f, 1.f, 0.f, 0.f,
+                        0.f, 0.f, 1.f, 0.f,
+                        0.f, 0.f, 0.f, 1.f,
+                };
+
         glUseProgram(program);
-        glBindVertexArray(vao);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
-
-        float scale_c = 0.7 ;
-        float cos_c = cos(time * 5) * scale_c;
-        float sin_c = sin(time * 5) * scale_c;
-
-        float aspect_ratio = (float)width  / (float)height;
-
-        float y = sin(time/10) / 2;
-        float x = cos(time/10) / 2;
-
-        float view[16] = {
-                1.f / aspect_ratio, 0.0, 0.0, 0.0,
-                0.0, 1.0, 0.0, 0.0,
-                0.0, 0.0, 1.0, 0.0,
-                0.0, 0.0, 0.0, 1.0,
-        };
-
-        float transform[16] = {
-                cos_c, -sin_c, 0.0, x,
-                sin_c, cos_c, 0.0, y,
-                0.0, 0.0, 1.0, 0.0,
-                0.0, 0.0, 0.0, 1.0
-        };
-
-        glUniformMatrix4fv(view_loc, 1, GL_TRUE, view);
-        glUniformMatrix4fv(transform_loc, 1, GL_TRUE, transform);
+        glUniformMatrix4fv(view_location, 1, GL_TRUE, view);
 
         SDL_GL_SwapWindow(window);
     }
